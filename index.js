c70479b712789f80ebdcd246434c4c2f187f89ca
unknown <mkrtchyanlarisa4@gmail.com>
2025-09-14 20:13:31 +0400

    add index.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+let name = "Larissa"; // arajin harci patasxan
+console.log(`hello ${1}`); // hello 1
+console.log (`hello {"name"}`); // hello name 
+console.log (`hello ${name}`); // hello Larissa
+
+let userName = 12345; // userName-i hamar string e anhrajesht
+let userAge = "шестандцать"; //userAge-y petq e tiv lini, ev ruseren cankali che grel
+let isOnline = "yes" // sa petq e boolean liner
+let score = true; // jisht e
