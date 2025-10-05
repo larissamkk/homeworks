1bf5633c4303fd371073f68b695cff9ee672cfc6
unknown <mkrtchyanlarisa4@gmail.com>
2025-10-05 19:59:06 +0400

    new homework

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-//4rd
-// const words = ["spray", "limit", "elite", "exuberant", "destruction", "present"]
-// result = words.filter((word)=> word.length >6);
-// console.log(result);
+// let arr = [1,2,3,4,5];
+// arr.splice(2,2);
+
+// let arr = [1,2,7,8];
+// arr.splice(0,3, 5,6,8);
+
+// function reversing(arr1, arr2,){
+//     connected = arr1.concat(arr2);
+ //    reversed =  connected.reverse();
+ //    return reversed;
+// }
+// a = [1,2,3]
+// b = [4,5,6];
+// console.log(reversing(a,b));
 
 
-//6rd
-// const people = [
-//     {name: "Anna", age:17},
-//     {name: "Karen", age: 20},
-//     {name: "Ella", age:12},
-//     {name:"Erik" age:30},
-// ]
-// result = people.find((person) =>person.age > 18 );
-// result =people.filter((person) => person.age >= 18);
-// result = people.map((person) => person.name);
-// console.log(result);
+// const fruits = "kiwi, banana, apple, melon".split(",");
+// console.log(fruits);
+
+
+// const arr = ["i", "learn", "js"];
+// const str = arr.join("");
+// console.log(str);
+
+
+// let arr = ["html", "css", "js"];
+// console.log(arr.includes("js"));
+
+
+// function hasString(arr,str){
+//     return arr.includes(str);
+// }
+// console.log(hasString(["apple", "banana"] "kiwi"));
+
+
+// let nums = [10,20,30,40,50,60];
+// nums.splice(1,3);
+// console.log(nums);
+
+
+// let text = "hi how are you";
+// let reversed = text.split("").reverse().join("");
+// console.log(reversed);
+
+
+// let animals = ["cat", "dog", "hamster", "parrot"];
+// let found = animals.find(word => word.includes("r"));
+// console.log(found);
